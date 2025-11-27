f6521f323d9a1f3882c2e830960fa98421bc5bca
Faqih Firman Pratama <faqihfirman.p@gmail.com>
2025-11-27 22:04:31 +0700

    feat:update  optimization tool

--- libs/optimizer.py
+++ libs/optimizer.py
@@ -4,7 +4,7 @@ from .union_find import UnionFind
 
 class RoadNetworkOptimizer:
     def __init__(self, village_names, optimization_mode='ratio'):
-     
+   
         self.village_names = village_names
         self.total_villages = len(village_names)
         self.potential_roads = []
@@ -15,30 +15,40 @@ class RoadNetworkOptimizer:
             for i in range(self.total_villages)
         }
 
-    def register_road_proposal(self, u, v, cost, benefit=1):
-       
-        if benefit == 0: benefit = 0.001
+
+    def register_road_proposal(self, u, v, distance_km, cost_rp, benefit_score):
         
+        if benefit_score == 0: benefit_score = 0.001
+
         if self.mode == 'distance':
-            weight = cost
+            weight = distance_km 
+        elif self.mode == 'cost':
+            weight = cost_rp    
         else:
-            weight = cost / benefit
+            weight = cost_rp / benefit_score 
         
         self.potential_roads.append({
             'u': u,
             'v': v,
-            'weight': weight,      
-            'real_cost': cost,      
-            'real_benefit': benefit 
+            'weight': weight,           
+            'real_cost': cost_rp,       
+            'real_benefit': benefit_score, 
+            'distance': distance_km     
         })
 
     def optimize_network_budget(self):
         solver = KruskalSolver(self.total_villages, self.potential_roads)
         mst_edges = solver.find_mst()
         
-        summary = {'total_cost': 0, 'total_benefit': 0}
+        summary = {
+            'total_cost': 0, 
+            'total_benefit': 0,
+            'total_distance': 0 
+        }
+        
         for edge in mst_edges:
             summary['total_cost'] += edge['real_cost']
             summary['total_benefit'] += edge['real_benefit']
+            summary['total_distance'] += edge['distance'] 
             
         return mst_edges, summary
